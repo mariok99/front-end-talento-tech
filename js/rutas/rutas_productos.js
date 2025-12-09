45a75c462266790470fcdbf9b3f90821c9e2c396
Mario <mlivia@dc.uba.ar>
2025-12-09 19:37:16 -0300

    prueba de ruta de data para ver si anda

--- js/rutas/rutas_productos.js
+++ js/rutas/rutas_productos.js
@@ -1,3 +1,3 @@
 const ruta_imagenes = '../img/';
-const ruta_productos_data = "../data/productos.json";
+const ruta_productos_data = "./data/productos.json";
 export { ruta_imagenes, ruta_productos_data };
