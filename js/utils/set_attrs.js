35c370bfd3fcee7303c00c5910c0e68e429647a1
Mario <mlivia@dc.uba.ar>
2025-12-09 20:25:35 -0300

    correción de nonbres de set_attrs

--- js/utils/set_attrs.js
+++ js/utils/set_attrs.js
@@ -1,4 +1,4 @@
-export function setAttrs(element, attrs) {
+export function set_attrs(element, attrs) {
     Object.keys(attrs).forEach((key) => {
         if (attrs[key] !== undefined) {
             element.setAttribute(key, attrs[key]);
